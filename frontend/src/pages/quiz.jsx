2fca1a009cca01bd994701ab164d5ea9b3470845
shashank soma <152509776+shashank0103@users.noreply.github.com>
2026-03-18 11:45:15 +0530

    Update quiz submission URL to production

--- frontend/src/pages/quiz.jsx
+++ frontend/src/pages/quiz.jsx
@@ -30,7 +30,7 @@ function Quiz() {
 
   const submitQuiz = async () => {
     try {
-      const res = await axios.post("http://localhost:5000/submit-quiz", {
+      const res = await axios.post("https://edwisely-9u5d.onrender.com/submit-quiz", {
         questions,
         answers,
       });
